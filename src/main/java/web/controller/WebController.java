3cdc8ec593cb48ec18e556a28062a3c7fb4886c3
Darko <darko.milovanovic@devoteam.com>
2019-07-25 22:17:44 +0200

    SonarLint minor changes

--- src/main/java/web/controller/WebController.java
+++ src/main/java/web/controller/WebController.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import redis.clients.jedis.Jedis;
-
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +12,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import redis.clients.jedis.Jedis;
 import utility.Utility;
 
 @RestController
@@ -31,11 +29,9 @@ public class WebController {
 	@Autowired
 	private Utility utils;
 
-
 	@GetMapping(value = "/readItems")
-	
+
 	public HashMap<String, String> readItems() {
-	
 
 		HashMap<String, String> res = (HashMap<String, String>) jedis.hgetAll("user:2");
 		return res;
@@ -44,7 +40,7 @@ public class WebController {
 	@GetMapping(value = "/readItems/{id}")
 
 	public HashMap<String, String> findItem(@PathVariable("id") final String id) {
-		
+
 		HashMap<String, String> res = (HashMap<String, String>) jedis.hgetAll("user:" + id);
 
 		return res;
@@ -55,7 +51,7 @@ public class WebController {
 	public ResponseEntity<HashMap<String, String>> createItems() {
 
 		for (Integer i = 0; i < 10; i++) {
-			HashMap<String, String> hmap = new HashMap<String, String>();
+			HashMap<String, String> hmap = new HashMap<>();
 			hmap.put("id", i.toString());
 			hmap.put("username", "User" + i);
 			hmap.put("password", "Password" + i);
@@ -73,10 +69,10 @@ public class WebController {
 		return new ResponseEntity<HashMap<String, String>>(HttpStatus.CREATED);
 	}
 
-@PostConstruct
+	@PostConstruct
 
-void auth() {
-	
-	jedis.auth(utils.getPassword());
-}
+	void auth() {
+
+		jedis.auth(utils.getPassword());
+	}
 }
